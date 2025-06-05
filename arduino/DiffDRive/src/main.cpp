80bfdba22bc288bff5166d6fc5e6beccbf9c822f
spbsyahiid <abdul.syahiid@spbgroup.com.my>
2025-06-05 13:43:58 +0800

    Added DIffDrive in Arduino1

--- arduino/DiffDRive/src/main.cpp
+++ arduino/DiffDRive/src/main.cpp
@@ -1,15 +1,52 @@
 #include <Arduino.h>
+#include <AFMotor.h>
+
+// Define all 4 motors
+AF_DCMotor motor1(1);  // M1
+AF_DCMotor motor2(2);  // M2
+AF_DCMotor motor3(3);  // M3
+AF_DCMotor motor4(4);  // M4
 
 void setup() {
-  pinMode(13, OUTPUT); // Built-in LED
-  pinMode(2, OUTPUT);
+  Serial.begin(9600);
+
+  // Set speed for all motors (0-255)
+  motor1.setSpeed(200);
+  motor2.setSpeed(200);
+  motor3.setSpeed(200);
+  motor4.setSpeed(200);
+}
+
+void moveForward() {
+  motor1.run(FORWARD);
+  motor2.run(FORWARD);
+  motor3.run(FORWARD);
+  motor4.run(FORWARD);
+}
+
+void moveBackward() {
+  motor1.run(BACKWARD);
+  motor2.run(BACKWARD);
+  motor3.run(BACKWARD);
+  motor4.run(BACKWARD);
+}
+
+void stopMotors() {
+  motor1.run(RELEASE);
+  motor2.run(RELEASE);
+  motor3.run(RELEASE);
+  motor4.run(RELEASE);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  digitalWrite(2, LOW);
-  delay(1000);
+  if (Serial.available()) {
+    char cmd = Serial.read();
+    if (cmd == 'F') {
+      moveForward();
+    } else if (cmd == 'B') {
+      moveBackward();
+    } else if (cmd == 'S') {
+      stopMotors();
+    }
+  }
 }
